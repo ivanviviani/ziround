da2a076df27630069c887849b0a09817f57392e5
ivanviviani <ivanviviani@users.noreply.github.com>
2020-08-03 14:55:04 +0200

    Update test_pre_mps.bat

--- test_pre_mps.bat
+++ test_pre_mps.bat
@@ -7,5 +7,5 @@ setlocal enabledelayedexpansion
 for %%f in (test_pre\*.pre) do ( 
 	::echo "fullname: %%f"
     ::echo "name: %%~nf"
-	cplex -c "read %%f" "write test_pre_mps\%%~nf_pre.mps"
+	cplex -c "read %%f" "write test_pre_mps\%%~nf.mps"
 )
